fd087bd3124abac911a68ca1213702fc31ee5d7b
mobin-newroz <ahmobin@newroztech.com>
2022-12-12 17:38:51 +0600

    chore : hibernate config add

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
